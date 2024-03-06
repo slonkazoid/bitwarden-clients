defa770ceea02a7ad5c11d7cdc74315fa85777cf
Andre Rosado <arosado@bitwarden.com>
2024-03-06 13:05:34 +0000

    Added tests to verify new error messages are working as expected and targetCollection protection is also working properly

---
+++ libs/importer/spec/test-data/bitwarden-json/unencrypted-org.json.ts
@@ -0,0 +1,38 @@
+export const unencryptedOrgExport = `
+{
+    "encrypted": false,
+    "collections": [
+      {
+        "id": "47f3b999-025b-4ce5-9762-b068011a6f6c",
+        "organizationId": "00c1eb40-e133-4e6c-999b-b068011a6d42",
+        "name": "Default collection",
+        "externalId": null
+      }
+    ],
+    "items": [
+      {
+        "passwordHistory": null,
+        "revisionDate": "2024-01-05T15:31:34.096Z",
+        "creationDate": "2024-01-05T15:31:34.096Z",
+        "deletedDate": null,
+        "id": "66599b35-195b-4589-ad3b-b0ee00ffdce7",
+        "organizationId": "00c1eb40-e133-4e6c-999b-b068011a6d42",
+        "folderId": null,
+        "type": 1,
+        "reprompt": 0,
+        "name": "one",
+        "notes": null,
+        "favorite": false,
+        "login": {
+          "fido2Credentials": [],
+          "uris": [],
+          "username": null,
+          "password": null,
+          "totp": null
+        },
+        "collectionIds": [
+          "47f3b999-025b-4ce5-9762-b068011a6f6c"
+        ]
+      }
+    ]
+  }`;
--- libs/importer/src/services/import.service.spec.ts
+++ libs/importer/src/services/import.service.spec.ts
@@ -3,18 +3,97 @@ import { mock, MockProxy } from "jest-mock-extended";
 import { CryptoService } from "@bitwarden/common/platform/abstractions/crypto.service";
 import { I18nService } from "@bitwarden/common/platform/abstractions/i18n.service";
 import { Utils } from "@bitwarden/common/platform/misc/utils";
+import { CollectionId, OrganizationId } from "@bitwarden/common/types/guid";
 import { CipherService } from "@bitwarden/common/vault/abstractions/cipher.service";
 import { CollectionService } from "@bitwarden/common/vault/abstractions/collection.service";
 import { FolderService } from "@bitwarden/common/vault/abstractions/folder/folder.service.abstraction";
+import { CipherRepromptType } from "@bitwarden/common/vault/enums/cipher-reprompt-type";
+import { CipherType } from "@bitwarden/common/vault/enums/cipher-type";
+import { FieldType } from "@bitwarden/common/vault/enums/field-type.enum";
+import { UriMatchType } from "@bitwarden/common/vault/enums/uri-match-type.enum";
+import { CipherData } from "@bitwarden/common/vault/models/data/cipher.data";
+import { CollectionData } from "@bitwarden/common/vault/models/data/collection.data";
+import { Cipher } from "@bitwarden/common/vault/models/domain/cipher";
+import { Collection } from "@bitwarden/common/vault/models/domain/collection";
 import { CollectionView } from "@bitwarden/common/vault/models/view/collection.view";
 import { FolderView } from "@bitwarden/common/vault/models/view/folder.view";
 
+import { unencryptedOrgExport } from "../../spec/test-data/bitwarden-json/unencrypted-org.json";
 import { BitwardenPasswordProtectedImporter } from "../importers/bitwarden/bitwarden-password-protected-importer";
 import { Importer } from "../importers/importer";
 import { ImportResult } from "../models/import-result";
 
 import { ImportApiServiceAbstraction } from "./import-api.service.abstraction";
 import { ImportService } from "./import.service";
+
+const cipherData: CipherData = {
+  id: "id",
+  organizationId: "1234",
+  folderId: "folderId",
+  edit: true,
+  viewPassword: true,
+  organizationUseTotp: true,
+  favorite: false,
+  revisionDate: "2022-01-31T12:00:00.000Z",
+  type: CipherType.Login,
+  name: "EncryptedString",
+  notes: "EncryptedString",
+  creationDate: "2022-01-01T12:00:00.000Z",
+  deletedDate: null,
+  key: "EncKey",
+  reprompt: CipherRepromptType.None,
+  login: {
+    uris: [{ uri: "EncryptedString", uriChecksum: "EncryptedString", match: UriMatchType.Domain }],
+    username: "EncryptedString",
+    password: "EncryptedString",
+    passwordRevisionDate: "2022-01-31T12:00:00.000Z",
+    totp: "EncryptedString",
+    autofillOnPageLoad: false,
+  },
+  passwordHistory: [{ password: "EncryptedString", lastUsedDate: "2022-01-31T12:00:00.000Z" }],
+  attachments: [
+    {
+      id: "a1",
+      url: "url",
+      size: "1100",
+      sizeName: "1.1 KB",
+      fileName: "file",
+      key: "EncKey",
+    },
+    {
+      id: "a2",
+      url: "url",
+      size: "1100",
+      sizeName: "1.1 KB",
+      fileName: "file",
+      key: "EncKey",
+    },
+  ],
+  fields: [
+    {
+      name: "EncryptedString",
+      value: "EncryptedString",
+      type: FieldType.Text,
+      linkedId: null,
+    },
+    {
+      name: "EncryptedString",
+      value: "EncryptedString",
+      type: FieldType.Hidden,
+      linkedId: null,
+    },
+  ],
+};
+
+const collectionData: CollectionData = {
+  id: "1234-1234" as CollectionId,
+  organizationId: "1234" as OrganizationId,
+  name: "collection123",
+  readOnly: false,
+  manage: true,
+  externalId: "",
+  hidePasswords: false,
+};
 
 describe("ImportService", () => {
   let importService: ImportService;
@@ -174,6 +253,64 @@ describe("ImportService", () => {
       expect(importResult.collections[0].name).toBe(myImportTarget);
       expect(importResult.collections[1].name).toBe(`${myImportTarget}/${mockCollection1.name}`);
       expect(importResult.collections[2].name).toBe(`${myImportTarget}/${mockCollection2.name}`);
+    });
+
+    it("if no targetCollection is found, does nothing", async () => {
+      collectionService.getAllDecrypted.mockResolvedValue([mockCollection1, mockCollection2]);
+
+      const myImportTarget = "myImportTarget";
+
+      importResult.collections.push(mockImportTargetCollection);
+
+      await importService["setImportTarget"](importResult, "123", myImportTarget);
+      expect(importResult.collections.length).toBe(1);
+      expect(importResult.collections[0].name).toBe("myImportTarget");
+    });
+  });
+
+  describe("Import", () => {
+    let importer: Importer;
+    const organizationId = Utils.newGuid();
+    const password = Utils.newGuid();
+    const promptForPassword_callback = async () => {
+      return password;
+    };
+
+    beforeEach(() => {
+      importer = importService.getImporter(
+        "bitwardenpasswordprotected",
+        promptForPassword_callback,
+        organizationId,
+      );
+    });
+
+    it("Should work flawlessly", async () => {
+      cipherService.encrypt.mockResolvedValue(new Cipher(cipherData));
+      collectionService.encrypt.mockResolvedValue(new Collection(collectionData));
+
+      await importService.import(importer, unencryptedOrgExport, "1234", null, true);
+    });
+
+    it("Should throw error building import data on null ciphers", async () => {
+      cipherService.encrypt.mockResolvedValue(undefined);
+      collectionService.encrypt.mockResolvedValue(new Collection(collectionData));
+
+      await expect(
+        importService.import(importer, unencryptedOrgExport, "1234", null, true),
+      ).rejects.toThrow(
+        "Unexpected error building import data: Cannot read properties of undefined (reading 'type')",
+      );
+    });
+
+    it("Should throw error building import data on null collections", async () => {
+      cipherService.encrypt.mockResolvedValue(new Cipher(cipherData));
+      collectionService.encrypt.mockResolvedValue(undefined);
+
+      await expect(
+        importService.import(importer, unencryptedOrgExport, "1234", null, true),
+      ).rejects.toThrow(
+        "Unexpected error building import data: Cannot set properties of undefined (setting 'organizationId')",
+      );
     });
   });
 });
