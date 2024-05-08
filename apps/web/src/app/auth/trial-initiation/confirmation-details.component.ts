d41aaff2da75f7a2504be24d5c2deb3e1ff8f4ea
nick-livefront <nick@livefront.com>
2024-05-08 15:22:36 -0500

    Allow confirmation details component to display secret manager confirmation

--- apps/web/src/app/auth/trial-initiation/confirmation-details.component.ts
+++ apps/web/src/app/auth/trial-initiation/confirmation-details.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from "@angular/core";
 
+import { Product } from "./finish-sign-up/enums/product";
+
 @Component({
   selector: "app-trial-confirmation-details",
   templateUrl: "confirmation-details.component.html",
@@ -7,4 +9,7 @@ import { Component, Input } from "@angular/core";
 export class ConfirmationDetailsComponent {
   @Input() email: string;
   @Input() orgLabel: string;
+  @Input() product?: Product = Product.PasswordManager;
+
+  protected readonly Product = Product;
 }
