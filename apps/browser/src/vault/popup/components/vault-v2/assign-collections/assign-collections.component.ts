900bb065e85855317942298b71d4156157bc4d2a
nick-livefront <nick@livefront.com>
2024-05-31 16:01:43 -0500

    stub out assign collection page

--- apps/browser/src/vault/popup/components/vault-v2/assign-collections/assign-collections.component.ts
+++ apps/browser/src/vault/popup/components/vault-v2/assign-collections/assign-collections.component.ts
@@ -1,8 +1,32 @@
+import { Location } from "@angular/common";
 import { Component } from "@angular/core";
+
+import { JslibModule } from "@bitwarden/angular/jslib.module";
+import { ButtonModule } from "@bitwarden/components";
+
+
+import { PopOutComponent } from "../../../../../platform/popup/components/pop-out.component";
+import { PopupFooterComponent } from "../../../../../platform/popup/layout/popup-footer.component";
+import { PopupHeaderComponent } from "../../../../../platform/popup/layout/popup-header.component";
+import { PopupPageComponent } from "../../../../../platform/popup/layout/popup-page.component";
 
 @Component({
   standalone: true,
   selector: "app-assign-collections",
   templateUrl: "./assign-collections.component.html",
+  imports: [
+    JslibModule,
+    ButtonModule,
+    PopupPageComponent,
+    PopupHeaderComponent,
+    PopupFooterComponent,
+    PopOutComponent,
+  ],
 })
-export class AssignCollections {}
+export class AssignCollections {
+  constructor(private location: Location) {}
+
+  cancel() {
+    this.location.back();
+  }
+}
