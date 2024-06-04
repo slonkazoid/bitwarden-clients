2e14ce4c58336137186f5f6dbce6c53aa188426f
Bernd Schoolmann <mail@quexten.com>
2024-06-04 14:32:55 +0200

    Move constants to top of file

--- apps/desktop/desktop_native/src/process_isolation/linux.rs
+++ apps/desktop/desktop_native/src/process_isolation/linux.rs
@@ -1,10 +1,17 @@
 use anyhow::Result;
-use libc;
+use libc::{c_int, self};
+
+// RLIMIT_CORE is the maximum size of a core dump file. Setting both to 0 disables core dumps, on crashes
+// https://github.com/torvalds/linux/blob/1613e604df0cd359cf2a7fbd9be7a0bcfacfabd0/include/uapi/asm-generic/resource.h#L20
+const RLIMIT_CORE: c_int = 4;
+
+// PR_SET_DUMPABLE makes it so no other running process (root or same user) can dump the memory of this process
+// or attach a debugger to it.
+// https://github.com/torvalds/linux/blob/a38297e3fb012ddfa7ce0321a7e5a8daeb1872b6/include/uapi/linux/prctl.h#L14
+const PR_SET_DUMPABLE: c_int = 4;
 
 pub fn disable_coredumps() -> Result<()> {
-    // RLIMIT_CORE is the maximum size of a core dump file. Setting both to 0 disables core dumps, on crashes
-    // https://github.com/torvalds/linux/blob/1613e604df0cd359cf2a7fbd9be7a0bcfacfabd0/include/uapi/asm-generic/resource.h#L20
-    const RLIMIT_CORE: u32 = 4;
+
     let rlimit = libc::rlimit {
         rlim_cur: 0,
         rlim_max: 0,
@@ -18,7 +25,6 @@ pub fn disable_coredumps() -> Result<()> {
 }
 
 pub fn is_core_dumping_disabled() -> Result<bool> {
-    const RLIMIT_CORE: u32 = 4;
     let mut rlimit = libc::rlimit {
         rlim_cur: 0,
         rlim_max: 0,
@@ -32,10 +38,6 @@ pub fn is_core_dumping_disabled() -> Result<bool> {
 }
 
 pub fn disable_memory_access() -> Result<()> {
-    // PR_SET_DUMPABLE makes it so no other running process (root or same user) can dump the memory of this process
-    // or attach a debugger to it.
-    // https://github.com/torvalds/linux/blob/a38297e3fb012ddfa7ce0321a7e5a8daeb1872b6/include/uapi/linux/prctl.h#L14
-    const PR_SET_DUMPABLE: i32 = 4;
     if unsafe { libc::prctl(PR_SET_DUMPABLE, 0) } != 0 {
         let e = std::io::Error::last_os_error();
         return Err(anyhow::anyhow!("failed to disable memory dumping, memory is dumpable by other processes {}", e))
