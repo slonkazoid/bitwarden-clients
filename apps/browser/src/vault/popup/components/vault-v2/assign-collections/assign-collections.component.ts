bc6344c2a0faac5b1cdca9937891924f04d1c554
nick-livefront <nick@livefront.com>
2024-06-04 11:26:22 -0500

    add transfer of cipher to an organization

--- apps/browser/src/vault/popup/components/vault-v2/assign-collections/assign-collections.component.ts
+++ apps/browser/src/vault/popup/components/vault-v2/assign-collections/assign-collections.component.ts
@@ -1,17 +1,17 @@
 import { CommonModule, Location } from "@angular/common";
 import { Component } from "@angular/core";
 import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
-import { FormsModule } from "@angular/forms";
+import { FormBuilder, FormControl, ReactiveFormsModule } from "@angular/forms";
 import { ActivatedRoute } from "@angular/router";
-import { Observable, first, map, take, tap } from "rxjs";
+import { Observable, first, firstValueFrom, map, take, tap } from "rxjs";
 
 import { JslibModule } from "@bitwarden/angular/jslib.module";
 import { OrganizationService } from "@bitwarden/common/admin-console/abstractions/organization/organization.service.abstraction";
 import { OrganizationUserStatusType } from "@bitwarden/common/admin-console/enums";
 import { Organization } from "@bitwarden/common/admin-console/models/domain/organization";
 import { I18nService } from "@bitwarden/common/platform/abstractions/i18n.service";
+import { LogService } from "@bitwarden/common/platform/abstractions/log.service";
 import { Utils } from "@bitwarden/common/platform/misc/utils";
-import { Checkable } from "@bitwarden/common/types/checkable";
 import { CipherService } from "@bitwarden/common/vault/abstractions/cipher.service";
 import { CollectionService } from "@bitwarden/common/vault/abstractions/collection.service";
 import { CipherView } from "@bitwarden/common/vault/models/view/cipher.view";
@@ -22,12 +22,17 @@ import {
   SelectModule,
   FormFieldModule,
   SelectItemView,
-} from "@bitwarden/components";
+ ToastService } from "@bitwarden/components";
 
 import { PopOutComponent } from "../../../../../platform/popup/components/pop-out.component";
 import { PopupFooterComponent } from "../../../../../platform/popup/layout/popup-footer.component";
 import { PopupHeaderComponent } from "../../../../../platform/popup/layout/popup-header.component";
 import { PopupPageComponent } from "../../../../../platform/popup/layout/popup-page.component";
+
+type TransferForm = {
+  organizationId: FormControl<string | null>;
+  collections: FormControl<SelectItemView[]>;
+};
 
 @Component({
   standalone: true,
@@ -38,8 +43,8 @@ import { PopupPageComponent } from "../../../../../platform/popup/layout/popup-p
     JslibModule,
     ButtonModule,
     SelectModule,
-    FormsModule,
     FormFieldModule,
+    ReactiveFormsModule,
     PopupPageComponent,
     PopupHeaderComponent,
     PopupFooterComponent,
@@ -49,13 +54,10 @@ import { PopupPageComponent } from "../../../../../platform/popup/layout/popup-p
 })
 export class AssignCollections {
   /** All writeable collections */
-  private writeableCollections: Checkable<CollectionView>[] = [];
+  private writeableCollections: CollectionView[] = [];
 
   /** Available organizations to which the cipher can be assigned to */
   organizations$: Observable<Organization[]>;
-
-  /** The selected organization Id */
-  organizationId: string | null = null;
 
   /** Selectable collections formatted to use with the MultiSelectComponent */
   collections: SelectItemView[] = [];
@@ -63,12 +65,23 @@ export class AssignCollections {
   /** The current cipher being altered */
   cipher: CipherView;
 
+  /** Promise associated with the submission form, used with `appApiAction` */
+  formPromise: Promise<void>;
+
+  transferForm = this.formBuilder.group<TransferForm>({
+    organizationId: new FormControl(null),
+    collections: new FormControl([]),
+  });
+
   constructor(
     private location: Location,
     private collectionService: CollectionService,
     private organizationService: OrganizationService,
     private i18nService: I18nService,
     private cipherService: CipherService,
+    private formBuilder: FormBuilder,
+    private toastService: ToastService,
+    private logService: LogService,
     route: ActivatedRoute,
   ) {
     // eslint-disable-next-line rxjs/no-async-subscribe
@@ -93,24 +106,31 @@ export class AssignCollections {
           .sort(Utils.getSortFunction(this.i18nService, "name"));
       }),
       tap((orgs) => {
+        const { organizationId } = this.transferForm.controls;
         // If there is only one organization, select it by default
-        if (this.organizationId === null && orgs.length > 0) {
-          this.organizationId = orgs[0].id;
-          this.updateCollections();
+        if (organizationId.value === null && orgs.length === 1) {
+          this.transferForm.controls.organizationId.setValue(orgs[0].id);
+          this.transferForm.controls.organizationId.disable();
         }
       }),
     );
+
+    this.transferForm.controls.organizationId.valueChanges
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => {
+        this.updateCollections();
+      });
   }
 
   /** Populate the selectable collections based on the organization selected */
   updateCollections() {
-    this.writeableCollections.forEach((c) => (c.checked = false));
+    const { organizationId } = this.transferForm.controls;
 
-    if (this.organizationId === null || this.writeableCollections.length === 0) {
+    if (organizationId.value === null || this.writeableCollections.length === 0) {
       this.collections = [];
     } else {
       this.collections = this.writeableCollections
-        .filter((c) => c.organizationId === this.organizationId)
+        .filter((c) => c.organizationId === organizationId.value)
         .map((c) => ({
           id: c.id,
           value: c.id,
@@ -119,6 +139,49 @@ export class AssignCollections {
           icon: "bwi-collection",
         }));
     }
+  }
+
+  async submit() {
+    const { collections, organizationId } = this.transferForm.getRawValue();
+    const collectionIds = collections.map((c) => c.id);
+
+    if (collectionIds.length === 0) {
+      this.toastService.showToast({
+        variant: "error",
+        title: this.i18nService.t("errorOccurred"),
+        message: this.i18nService.t("selectOneCollection"),
+      });
+      return;
+    }
+
+    const cipherDomain = await this.cipherService.get(this.cipher.id);
+    const cipherView = await cipherDomain.decrypt(
+      await this.cipherService.getKeyForCipherKeyDecryption(cipherDomain),
+    );
+    const orgs = await firstValueFrom(this.organizations$);
+    const orgName =
+      orgs.find((o) => o.id === organizationId)?.name ?? this.i18nService.t("organization");
+
+    try {
+      this.formPromise = this.cipherService.shareWithServer(
+        cipherView,
+        organizationId,
+        collectionIds,
+      );
+
+      await this.formPromise;
+
+      this.navigateBack();
+      this.toastService.showToast({
+        variant: "success",
+        title: null,
+        message: this.i18nService.t("movedItemToOrg", cipherView.name, orgName),
+      });
+      return true;
+    } catch (e) {
+      this.logService.error(e);
+    }
+    return false;
   }
 
   /** Navigates the user back to the previous screen */
