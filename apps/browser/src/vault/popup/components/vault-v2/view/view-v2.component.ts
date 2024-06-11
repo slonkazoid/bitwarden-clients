cd64807e0ae312a0894e69320c43416bf81ded80
jng <jng@bitwarden.com>
2024-06-11 15:04:25 -0400

    update footer with trash icon. bring in cipher data for item details

--- apps/browser/src/vault/popup/components/vault-v2/view/view-v2.component.ts
+++ apps/browser/src/vault/popup/components/vault-v2/view/view-v2.component.ts
@@ -1,14 +1,21 @@
 import { CommonModule } from "@angular/common";
-import { Component } from "@angular/core";
+import { Component, OnDestroy } from "@angular/core";
 import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
 import { FormsModule } from "@angular/forms";
 import { ActivatedRoute, Router } from "@angular/router";
+import { Subject, firstValueFrom, takeUntil } from "rxjs";
 
 import { JslibModule } from "@bitwarden/angular/jslib.module";
+import { OrganizationService } from "@bitwarden/common/admin-console/abstractions/organization/organization.service.abstraction";
+import { Organization } from "@bitwarden/common/admin-console/models/domain/organization";
 import { I18nService } from "@bitwarden/common/platform/abstractions/i18n.service";
 import { CipherService } from "@bitwarden/common/vault/abstractions/cipher.service";
+import { CollectionService } from "@bitwarden/common/vault/abstractions/collection.service";
+import { FolderService } from "@bitwarden/common/vault/abstractions/folder/folder.service.abstraction";
 import { CipherType } from "@bitwarden/common/vault/enums";
 import { CipherView } from "@bitwarden/common/vault/models/view/cipher.view";
+import { CollectionView } from "@bitwarden/common/vault/models/view/collection.view";
+import { FolderView } from "@bitwarden/common/vault/models/view/folder.view";
 import { SearchModule, ButtonModule } from "@bitwarden/components";
 
 import { PopupFooterComponent } from "../../../../../platform/popup/layout/popup-footer.component";
@@ -30,18 +37,30 @@ import { PopupPageComponent } from "../../../../../platform/popup/layout/popup-p
     PopupFooterComponent,
   ],
 })
-export class ViewV2Component {
+export class ViewV2Component implements OnDestroy {
   headerText: string;
   cipherId: string;
   cipher: CipherView;
+  organization: Organization;
+  cipherCollections: CollectionView[];
+  folder: FolderView;
+  private destroyed$: Subject<void> = new Subject();
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
     private i18nService: I18nService,
     private cipherService: CipherService,
+    private organizationService: OrganizationService,
+    private collectionService: CollectionService,
+    private folderService: FolderService,
   ) {
     this.subscribeToParams();
+  }
+
+  ngOnDestroy(): void {
+    this.destroyed$.next();
+    this.destroyed$.complete();
   }
 
   subscribeToParams(): void {
@@ -73,9 +92,36 @@ export class ViewV2Component {
     this.cipher = await cipher.decrypt(
       await this.cipherService.getKeyForCipherKeyDecryption(cipher),
     );
+
+    if (this.cipher.collectionIds.length > 0) {
+      const allCollections = await this.collectionService.getAllDecrypted();
+
+      this.cipherCollections = allCollections.filter((collection) => {
+        if (this.cipher.collectionIds.includes(collection.id)) {
+          return collection;
+        }
+      });
+    }
+
+    if (this.cipher.organizationId) {
+      this.organizationService
+        .get$(this.cipher.organizationId)
+        .pipe(takeUntil(this.destroyed$))
+        .subscribe((org) => {
+          this.organization = org;
+        });
+    }
+
+    if (this.cipher.folderId) {
+      if (this.cipher.folderId) {
+        this.folder = await (
+          await firstValueFrom(this.folderService.folderViews$)
+        ).find((f) => f.id == this.cipher.folderId);
+      }
+    }
   }
 
-  async edit() {
+  async editCipher() {
     if (this.cipher.isDeleted) {
       return false;
     }
@@ -87,4 +133,6 @@ export class ViewV2Component {
     });
     return true;
   }
+
+  deleteCipher() {}
 }
