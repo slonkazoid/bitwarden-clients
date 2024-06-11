2cd1032a82574d4fa96cbf911866f3f14cc5783a
cd-bitwarden <106776772+cd-bitwarden@users.noreply.github.com>
2024-06-11 13:34:45 -0400

    moving files to better folder, also fixing UI

--- apps/web/src/app/secrets-manager/secrets-manager-landing/request-sm-access.component.ts
+++ apps/web/src/app/secrets-manager/secrets-manager-landing/request-sm-access.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from "@angular/core";
 import { FormControl, FormGroup, Validators } from "@angular/forms";
 import { Router } from "@angular/router";
 
-
 import { ApiService } from "@bitwarden/common/abstractions/api.service";
 import { OrganizationService } from "@bitwarden/common/admin-console/abstractions/organization/organization.service.abstraction";
 import { Organization } from "@bitwarden/common/admin-console/models/domain/organization";
