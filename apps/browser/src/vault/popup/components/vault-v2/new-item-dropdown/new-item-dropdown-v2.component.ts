7e509841d1cabc5010f1384b9a116862d8b9a558
nick-livefront <nick@livefront.com>
2024-06-12 11:04:43 -0500

    pass along selected vault in new item dropdown

--- apps/browser/src/vault/popup/components/vault-v2/new-item-dropdown/new-item-dropdown-v2.component.ts
+++ apps/browser/src/vault/popup/components/vault-v2/new-item-dropdown/new-item-dropdown-v2.component.ts
@@ -1,10 +1,16 @@
 import { CommonModule } from "@angular/common";
 import { Component, OnDestroy, OnInit } from "@angular/core";
+import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
 import { Router, RouterLink } from "@angular/router";
 
 import { JslibModule } from "@bitwarden/angular/jslib.module";
 import { CipherType } from "@bitwarden/common/vault/enums";
 import { ButtonModule, NoItemsModule, MenuModule } from "@bitwarden/components";
+
+import {
+  MY_VAULT_ID,
+  VaultPopupListFiltersService,
+} from "../../../services/vault-popup-list-filters.service";
 
 @Component({
   selector: "app-new-item-dropdown",
@@ -13,16 +19,29 @@ import { ButtonModule, NoItemsModule, MenuModule } from "@bitwarden/components";
   imports: [NoItemsModule, JslibModule, CommonModule, ButtonModule, RouterLink, MenuModule],
 })
 export class NewItemDropdownV2Component implements OnInit, OnDestroy {
+  private selectedVaultId: string | null | undefined = null;
+
   cipherType = CipherType;
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    vaultPopupListFiltersService: VaultPopupListFiltersService,
+  ) {
+    vaultPopupListFiltersService.filters$.pipe(takeUntilDestroyed()).subscribe((filters) => {
+      this.selectedVaultId =
+        filters.organization?.id !== MY_VAULT_ID ? filters.organization?.id : null;
+    });
+  }
 
   ngOnInit(): void {}
 
   ngOnDestroy(): void {}
 
-  // TODO PM-6826: add selectedVault query param
   newItemNavigate(type: CipherType) {
-    void this.router.navigate(["/add-cipher"], { queryParams: { type: type, isNew: true } });
+    const selectedVault = this.selectedVaultId;
+
+    void this.router.navigate(["/add-cipher"], {
+      queryParams: { type: type, isNew: true, selectedVault },
+    });
   }
 }
