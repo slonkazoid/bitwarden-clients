9ea21a97a73dd1f5ec02f9290194142c8a2577c6
Jared Snider <jsnider@bitwarden.com>
2024-06-15 14:10:00 -0400

    PM-7321 - update input password based on latest PR changes to test.

--- libs/auth/src/angular/input-password/input-password.component.ts
+++ libs/auth/src/angular/input-password/input-password.component.ts
@@ -1,14 +1,12 @@
 import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
 import { ReactiveFormsModule, FormBuilder, Validators } from "@angular/forms";
-import { firstValueFrom, map } from "rxjs";
 
 import { JslibModule } from "@bitwarden/angular/jslib.module";
 import { AuditService } from "@bitwarden/common/abstractions/audit.service";
 import { PolicyApiServiceAbstraction } from "@bitwarden/common/admin-console/abstractions/policy/policy-api.service.abstraction";
 import { PolicyService } from "@bitwarden/common/admin-console/abstractions/policy/policy.service.abstraction";
 import { MasterPasswordPolicyOptions } from "@bitwarden/common/admin-console/models/domain/master-password-policy-options";
-import { AccountService } from "@bitwarden/common/auth/abstractions/account.service";
-import { KdfConfigService } from "@bitwarden/common/auth/abstractions/kdf-config.service";
+import { PBKDF2KdfConfig } from "@bitwarden/common/auth/models/domain/kdf-config";
 import { CryptoService } from "@bitwarden/common/platform/abstractions/crypto.service";
 import { I18nService } from "@bitwarden/common/platform/abstractions/i18n.service";
 import { Utils } from "@bitwarden/common/platform/misc/utils";
@@ -29,7 +27,8 @@ import { PasswordCalloutComponent } from "../password-callout/password-callout.c
 
 export interface PasswordInputResult {
   masterKeyHash: string;
-  hint?: string;
+  kdfConfig: PBKDF2KdfConfig;
+  hint: string;
 }
 
 @Component({
@@ -52,13 +51,13 @@ export interface PasswordInputResult {
 export class InputPasswordComponent implements OnInit {
   @Output() onPasswordFormSubmit = new EventEmitter();
 
+  @Input({ required: true }) email: string;
   @Input() protected buttonText: string;
   @Input() private orgId: string;
 
   private minHintLength = 0;
   protected maxHintLength = 50;
 
-  protected email: string;
   protected minPasswordLength = Utils.minimumPasswordLength;
   protected masterPasswordPolicy: MasterPasswordPolicyOptions;
   protected passwordStrengthResult: any;
@@ -94,22 +93,17 @@ export class InputPasswordComponent implements OnInit {
   );
 
   constructor(
-    private accountService: AccountService,
     private auditService: AuditService,
     private cryptoService: CryptoService,
     private dialogService: DialogService,
     private formBuilder: FormBuilder,
     private i18nService: I18nService,
-    private kdfConfigService: KdfConfigService,
     private policyService: PolicyService,
     private toastService: ToastService,
     private policyApiService: PolicyApiServiceAbstraction,
   ) {}
 
   async ngOnInit() {
-    this.email = await firstValueFrom(
-      this.accountService.activeAccount$.pipe(map((a) => a?.email)),
-    );
     this.masterPasswordPolicy = await this.policyApiService.getMasterPasswordPolicyOptsForOrgUser(
       this.orgId,
     );
@@ -127,10 +121,12 @@ export class InputPasswordComponent implements OnInit {
       return;
     }
 
+    const password = this.formGroup.controls.password.value;
+
     // Check if password is breached (if breached, user chooses to accept and continue or not)
     const passwordIsBreached =
       this.formGroup.controls.checkForBreaches.value &&
-      (await this.auditService.passwordLeaked(this.formGroup.controls.password.value));
+      (await this.auditService.passwordLeaked(password));
 
     if (passwordIsBreached) {
       const userAcceptedDialog = await this.dialogService.openSimpleDialog({
@@ -149,7 +145,7 @@ export class InputPasswordComponent implements OnInit {
       this.masterPasswordPolicy != null &&
       !this.policyService.evaluateMasterPassword(
         this.passwordStrengthResult.score,
-        this.formGroup.controls.password.value,
+        password,
         this.masterPasswordPolicy,
       )
     ) {
@@ -163,21 +159,17 @@ export class InputPasswordComponent implements OnInit {
     }
 
     // Create and hash new master key
-    const masterPassword = this.formGroup.controls.password.value;
-    const email = await firstValueFrom(
-      this.accountService.activeAccount$.pipe(map((a) => a?.email)),
-    );
-    const kdfConfig = await this.kdfConfigService.getKdfConfig();
-
-    const newMasterKey = await this.cryptoService.makeMasterKey(
-      masterPassword,
-      email.trim().toLowerCase(),
+    const kdfConfig = new PBKDF2KdfConfig();
+    const masterKey = await this.cryptoService.makeMasterKey(
+      password,
+      this.email.trim().toLowerCase(),
       kdfConfig,
     );
-    const newMasterKeyHash = await this.cryptoService.hashMasterKey(masterPassword, newMasterKey);
+    const masterKeyHash = await this.cryptoService.hashMasterKey(password, masterKey);
 
     const passwordInputResult = {
-      masterKeyHash: newMasterKeyHash,
+      masterKeyHash,
+      kdfConfig,
       hint: this.formGroup.controls.hint.value,
     };
 
