e973e6917d781ba0e7b060f8c8171e40be177d49
rr-bw <102181210+rr-bw@users.noreply.github.com>
2024-06-18 10:50:01 -0700

    add InputPasswordComponent to template

--- apps/web/src/app/auth/set-password-v2.component.ts
+++ apps/web/src/app/auth/set-password-v2.component.ts
@@ -2,13 +2,14 @@ import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
 import { firstValueFrom } from "rxjs";
 
-import { InputPasswordComponent } from "@bitwarden/auth/angular";
+import { JslibModule } from "@bitwarden/angular/jslib.module";
+import { InputPasswordComponent, PasswordInputResult } from "@bitwarden/auth/angular";
 
 @Component({
   standalone: true,
   selector: "app-set-password-v2",
   templateUrl: "./set-password-v2.component.html",
-  imports: [InputPasswordComponent],
+  imports: [InputPasswordComponent, JslibModule],
 })
 export class SetPasswordV2Component implements OnInit {
   orgName: string;
@@ -28,5 +29,9 @@ export class SetPasswordV2Component implements OnInit {
     }
 
     await this.router.navigate(["/"]);
+  }
+
+  getPasswordInputResult(passwordInputResult: PasswordInputResult) {
+    console.log(passwordInputResult);
   }
 }
