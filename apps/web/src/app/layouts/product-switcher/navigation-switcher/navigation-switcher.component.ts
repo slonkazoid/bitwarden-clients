70c448bfd3f3789aff285d802a4c4751349029c9
cd-bitwarden <106776772+cd-bitwarden@users.noreply.github.com>
2024-06-18 13:00:59 -0400

    Fixing logic to match top bar

--- apps/web/src/app/layouts/product-switcher/navigation-switcher/navigation-switcher.component.ts
+++ apps/web/src/app/layouts/product-switcher/navigation-switcher/navigation-switcher.component.ts
@@ -13,29 +13,16 @@ export class NavigationProductSwitcherComponent {
   protected readonly accessibleProducts$: Observable<ProductSwitcherItem[]> =
     this.productSwitcherService.products$.pipe(map((products) => products.bento ?? []));
 
-  protected readonly moreProductsInternal$: Observable<ProductSwitcherItem[]> =
-    this.productSwitcherService.products$.pipe(
-      map((products) => products.other ?? []),
-      // Ensure that organizations is displayed first in the other products list
-      // This differs from the order in `ProductSwitcherContentComponent` but matches the intent
-      // from product & design
-      map((products) =>
-        products
-          .filter((product) => product.name === "Secrets Manager")
-          .sort((product) => (product.name === "Organizations" ? -1 : 1)),
-      ),
-    );
-
   protected readonly moreProducts$: Observable<ProductSwitcherItem[]> =
     this.productSwitcherService.products$.pipe(
       map((products) => products.other ?? []),
       // Ensure that organizations is displayed first in the other products list
       // This differs from the order in `ProductSwitcherContentComponent` but matches the intent
       // from product & design
-      map((products) =>
-        products
-          .filter((product) => product.name !== "Secrets Manager")
-          .sort((product) => (product.name === "Organizations" ? -1 : 1)),
-      ),
+      map((products) => products.sort((product) => (product.name === "Organizations" ? -1 : 1))),
     );
+
+  isInternalMarketingRoute(marketingRoute: any) {
+    return marketingRoute.startsWith("/");
+  }
 }
