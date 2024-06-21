d99ae7d54725a6627f98647fde7083cc8b6a45ba
Conner Turnbull <cturnbull@bitwarden.com>
2024-06-21 14:39:28 -0400

    Removed whitespace from email form when setting up families sponsorship

--- apps/web/src/app/admin-console/settings/sponsored-families.component.ts
+++ apps/web/src/app/admin-console/settings/sponsored-families.component.ts
@@ -19,6 +19,8 @@ import { I18nService } from "@bitwarden/common/platform/abstractions/i18n.servic
 import { PlatformUtilsService } from "@bitwarden/common/platform/abstractions/platform-utils.service";
 import { SyncService } from "@bitwarden/common/vault/abstractions/sync/sync.service.abstraction";
 
+import { trimEmailValidator } from "./validators/trim-email.validator";
+
 interface RequestSponsorshipForm {
   selectedSponsorshipOrgId: FormControl<string>;
   sponsorshipEmail: FormControl<string>;
@@ -57,7 +59,7 @@ export class SponsoredFamiliesComponent implements OnInit, OnDestroy {
         validators: [Validators.required],
       }),
       sponsorshipEmail: new FormControl("", {
-        validators: [Validators.email, Validators.required],
+        validators: [Validators.email, Validators.required, trimEmailValidator()],
         asyncValidators: [
           this.notAllowedValueAsync(
             () => firstValueFrom(this.accountService.activeAccount$.pipe(map((a) => a?.email))),
