0296e832653e342d62dd017e66ee8769f8496c10
jng <jng@bitwarden.com>
2024-06-24 18:54:50 -0400

    reformat item details and custom fields. removed excess code. simplified html

--- apps/browser/src/vault/popup/components/vault-v2/view/custom-fields/custom-fields-v2.component.ts
+++ apps/browser/src/vault/popup/components/vault-v2/view/custom-fields/custom-fields-v2.component.ts
@@ -1,12 +1,11 @@
 import { CommonModule } from "@angular/common";
 import { Component, Input, OnInit } from "@angular/core";
-import { FormsModule } from "@angular/forms";
 
 import { JslibModule } from "@bitwarden/angular/jslib.module";
 import { I18nService } from "@bitwarden/common/platform/abstractions/i18n.service";
 import { FieldType, LinkedIdType, LoginLinkedId } from "@bitwarden/common/vault/enums";
 import { FieldView } from "@bitwarden/common/vault/models/view/field.view";
-import { SearchModule, ButtonModule, CardComponent } from "@bitwarden/components";
+import { CardComponent } from "@bitwarden/components";
 
 type HiddenFieldMap = {
   [key: string]: boolean;
@@ -16,7 +15,7 @@ type HiddenFieldMap = {
   selector: "app-custom-fields-v2",
   templateUrl: "custom-fields-v2.component.html",
   standalone: true,
-  imports: [CommonModule, SearchModule, JslibModule, FormsModule, ButtonModule, CardComponent],
+  imports: [CommonModule, JslibModule, CardComponent],
 })
 export class CustomFieldV2Component implements OnInit {
   @Input() fields: FieldView[];
--- apps/browser/src/vault/popup/components/vault-v2/view/item-details/item-details-v2.component.ts
+++ apps/browser/src/vault/popup/components/vault-v2/view/item-details/item-details-v2.component.ts
@@ -1,19 +1,18 @@
 import { CommonModule } from "@angular/common";
 import { Component, Input } from "@angular/core";
-import { FormsModule } from "@angular/forms";
 
 import { JslibModule } from "@bitwarden/angular/jslib.module";
 import { Organization } from "@bitwarden/common/admin-console/models/domain/organization";
 import { CipherView } from "@bitwarden/common/vault/models/view/cipher.view";
 import { CollectionView } from "@bitwarden/common/vault/models/view/collection.view";
 import { FolderView } from "@bitwarden/common/vault/models/view/folder.view";
-import { SearchModule, ButtonModule, CardComponent } from "@bitwarden/components";
+import { CardComponent } from "@bitwarden/components";
 
 @Component({
   selector: "app-item-details-v2",
   templateUrl: "item-details-v2.component.html",
   standalone: true,
-  imports: [CommonModule, SearchModule, JslibModule, FormsModule, ButtonModule, CardComponent],
+  imports: [CommonModule, JslibModule, CardComponent],
 })
 export class ItemDetailsV2Component {
   @Input() cipher: CipherView;
