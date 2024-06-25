0b1c2f763570552850537ed5ee0c415c5a1ce7b2
Bernd Schoolmann <mail@quexten.com>
2024-06-25 11:18:02 +0200

    Ensure loggedOut is sent for non-active user logouts too

--- apps/desktop/src/app/app.component.ts
+++ apps/desktop/src/app/app.component.ts
@@ -686,9 +686,7 @@ export class AppComponent implements OnInit, OnDestroy {
 
     // This must come last otherwise the logout will prematurely trigger
     // a process reload before all the state service user data can be cleaned up
-    if (userBeingLoggedOut === activeUserId) {
-      this.authService.logOut(async () => {});
-    }
+    this.authService.logOut(async () => {});
   }
 
   private async recordActivity() {
