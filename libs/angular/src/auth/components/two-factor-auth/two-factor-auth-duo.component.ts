2cd0b4756a76a111fba96bc855f6d59e30f1a8e2
Bernd Schoolmann <mail@quexten.com>
2024-06-26 12:04:47 +0200

    Remove duo v2

--- libs/angular/src/auth/components/two-factor-auth/two-factor-auth-duo.component.ts
+++ libs/angular/src/auth/components/two-factor-auth/two-factor-auth-duo.component.ts
@@ -2,7 +2,6 @@ import { DialogModule } from "@angular/cdk/dialog";
 import { CommonModule } from "@angular/common";
 import { Component, EventEmitter, Input, Output } from "@angular/core";
 import { ReactiveFormsModule, FormsModule } from "@angular/forms";
-import * as DuoWebSDK from "duo_web_sdk";
 
 import { JslibModule } from "@bitwarden/angular/jslib.module";
 import { I18nPipe } from "@bitwarden/angular/platform/pipes/i18n.pipe";
@@ -38,7 +37,6 @@ export class TwoFactorAuthDuoComponent {
   @Output() token = new EventEmitter<string>();
   @Input() providerData: any;
 
-  duoFrameless = false;
   duoFramelessUrl: string = null;
   duoResultListenerInitialized = false;
 
@@ -52,42 +50,15 @@ export class TwoFactorAuthDuoComponent {
   }
 
   async init() {
-    // 2 Duo 2FA flows available
-    // 1. Duo Web SDK (iframe) - existing, to be deprecated
-    // 2. Duo Frameless (new tab) - new
-
-    // AuthUrl only exists for new Duo Frameless flow
-    if (this.providerData.AuthUrl) {
-      this.duoFrameless = true;
-      // Setup listener for duo-redirect.ts connector to send back the code
-
-      if (!this.duoResultListenerInitialized) {
-        // setup client specific duo result listener
-        this.setupDuoResultListener();
-        this.duoResultListenerInitialized = true;
-      }
-
-      // flow must be launched by user so they can choose to remember the device or not.
-      this.duoFramelessUrl = this.providerData.AuthUrl;
-
-      await this.launchDuoFrameless();
-    } else {
-      // Duo Web SDK (iframe) flow
-      // TODO: remove when we remove the "duo-redirect" feature flag
-      setTimeout(() => {
-        DuoWebSDK.init({
-          iframe: undefined,
-          host: this.providerData.Host,
-          sig_request: this.providerData.Signature,
-          submit_callback: async (f: HTMLFormElement) => {
-            const sig = f.querySelector('input[name="sig_response"]') as HTMLInputElement;
-            if (sig != null) {
-              this.token.emit(sig.value);
-            }
-          },
-        });
-      }, 0);
+    // Setup listener for duo-redirect.ts connector to send back the code
+    if (!this.duoResultListenerInitialized) {
+      // setup client specific duo result listener
+      this.setupDuoResultListener();
+      this.duoResultListenerInitialized = true;
     }
+
+    // flow must be launched by user so they can choose to remember the device or not.
+    this.duoFramelessUrl = this.providerData.AuthUrl;
   }
 
   // Each client will have own implementation
