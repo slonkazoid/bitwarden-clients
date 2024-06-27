a70c228ce20f6755ea98a4f57f832606ced73df9
Andre Rosado <arosado@bitwarden.com>
2024-06-27 11:46:03 +0100

    replaced deprecated search code with searchControl

--- bitwarden_license/bit-web/src/app/tools/reports/member-access-report/member-access-report.component.ts
+++ bitwarden_license/bit-web/src/app/tools/reports/member-access-report/member-access-report.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from "@angular/core";
-import { BehaviorSubject, Subject, from, switchMap, takeUntil } from "rxjs";
+import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
+import { FormControl } from "@angular/forms";
+import { Subject, debounceTime } from "rxjs";
 
-import { SearchService } from "@bitwarden/common/abstractions/search.service";
 import { SearchModule, TableDataSource } from "@bitwarden/components";
 
 import { HeaderModule } from "../../../../../../../apps/web/src/app/layouts/header/header.module";
@@ -19,29 +20,17 @@ import { MemberAccessReportView } from "./view/member-access-report.view";
 export class MemberAccessReportComponent implements OnInit {
   protected destroy$ = new Subject<void>();
   protected dataSource = new TableDataSource<MemberAccessReportView>();
-  private _searchText$ = new BehaviorSubject<string>("");
+  protected searchControl = new FormControl("", { nonNullable: true });
 
-  get searchText() {
-    return this._searchText$.value;
+  constructor(protected reportService: MemberAccessReportService) {
+    // Connect the search input to the table dataSource filter input
+    this.searchControl.valueChanges
+      .pipe(debounceTime(200), takeUntilDestroyed())
+      .subscribe((v) => (this.dataSource.filter = v));
   }
-
-  set searchText(value: string) {
-    this._searchText$.next(value);
-    this.dataSource.filter = value;
-  }
-
-  constructor(
-    protected reportService: MemberAccessReportService,
-    private searchService: SearchService,
-  ) {}
 
   ngOnInit() {
     this.dataSource.data = this.reportService.getMemberAccessMockData();
-
-    this._searchText$.pipe(
-      switchMap((searchText) => from(this.searchService.isSearchable(searchText))),
-      takeUntil(this.destroy$),
-    );
   }
 
   ngOnDestroy() {
