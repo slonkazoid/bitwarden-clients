9787c9c52d53e8025369a1aa2db15dca151f520f
Andreas Coroiu <andreas.coroiu@gmail.com>
2024-07-02 17:16:21 +0200

    revert: awaiting floating promise
    gonna fix this in a separate PR

--- libs/common/spec/fake-storage.service.ts
+++ libs/common/spec/fake-storage.service.ts
@@ -84,7 +84,9 @@ export class FakeStorageService implements AbstractStorageService, ObservableSto
       );
     }
 
-    await this.mock.save(key, obj, options);
+    // FIXME: Verify that this floating promise is intentional. If it is, add an explanatory comment and ensure there is proper error handling.
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    this.mock.save(key, obj, options);
     this.store[key] = obj;
     this.updatesSubject.next({ key: key, updateType: "save" });
   }
