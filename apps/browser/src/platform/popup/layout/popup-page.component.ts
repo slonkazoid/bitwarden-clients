3f7973a40e5309fbf27c136b6153de9ab8fda636
Nick Krantz <nick@livefront.com>
2024-07-02 16:48:17 -0500

    fix duplicate scrollbar on popup page

--- apps/browser/src/platform/popup/layout/popup-page.component.ts
+++ apps/browser/src/platform/popup/layout/popup-page.component.ts
@@ -6,7 +6,7 @@ import { Component } from "@angular/core";
   templateUrl: "popup-page.component.html",
   standalone: true,
   host: {
-    class: "tw-h-full tw-flex tw-flex-col tw-flex-1 tw-overflow-y-auto",
+    class: "tw-h-full tw-flex tw-flex-col tw-flex-1 tw-overflow-y-hidden",
   },
   imports: [NgIf],
 })
