c764d34a10f0c904ac6d75137c0ad80e4cbce942
Bernd Schoolmann <mail@quexten.com>
2024-07-02 17:37:42 +0200

    Make unimplemented implementation panic for on_lock

--- apps/desktop/desktop_native/core/src/powermonitor/unimplemented.rs
+++ apps/desktop/desktop_native/core/src/powermonitor/unimplemented.rs
@@ -1,7 +1,5 @@
-use napi::threadsafe_function::{ErrorStrategy::CalleeHandled, ThreadsafeFunction};
-
-pub async fn on_lock(_: ThreadsafeFunction<(), CalleeHandled>) -> Result<(), Box<dyn std::error::Error>> {
-    return Ok(());
+pub async fn on_lock(_: tokio::sync::mpsc::Sender<()>) -> Result<(), Box<dyn std::error::Error>> {
+    unimplemented!();
 }
 
 pub async fn is_lock_monitor_available() -> bool {
